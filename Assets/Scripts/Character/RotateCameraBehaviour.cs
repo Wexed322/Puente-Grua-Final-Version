f314bfae2d3c604c65397cdf015a67fc5d886ecc
JostiN322 <jostinoriguela@gmail.com>
2022-05-04 23:50:01 -0500

    ss

---
+++ Assets/Scripts/Character/RotateCameraBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCameraBehaviour : MonoBehaviour
+{
+    public float sensivity;
+    void Update()
+    {
+        this.transform.Rotate(Vector3.up, sensivity);
+    }
+}
